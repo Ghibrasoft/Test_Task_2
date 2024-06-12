f0c1565728e939b2838f03caeab28e58f749220f
David <dgibradze@gmail.com>
2024-06-13 00:01:18 +0400

    Update: button_comp_styles

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -20,11 +20,11 @@ const Button: React.FC<IButtonProps> = ({
                 `hover:bg-vio-soft active:bg-vio hover:text-light-frost-pale border-[1px] border-frost-soft hover:border-vio-soft active:border-vio ${active ? 'bg-vio-soft border-vio-soft text-light-frost-pale' : 'text-vio-dark'}` :
                 type === 'secondary' ?
                     `ring-1 ring-vio-soft text-vio-soft hover:bg-vio-soft hover:text-light-frost-pale active:bg-vio ${active ? 'bg-vio-soft text-light-frost-pale' : ''}` :
-                    `text-vio-deep border-b-2 border-b-transparent hover:border-b-vio-soft rounded-none ${active ? 'font-bold text-vio-soft border-b-2 border-b-vio-soft' : ''}`;
+                    `text-vio-deep border-b-2 border-b-transparent hover:border-b-vio-soft rounded-none ${active ? 'font-bold text-vio-soft border-b-2 border-b-vio-soft' : 'text-vio-deep/50'}`;
 
     return (
         <button
-            className={`${typeClassName} ${className} flex items-center justify-center h-[38px] p-[15px] text-xs font-bold rounded-none sm:rounded-xl transition-all duration-100 ease-in`}
+            className={`${typeClassName} ${className} flex items-center justify-center h-[38px] px-3 py-1 sm:p-[15px] text-sm sm:text-xs font-bold rounded-none sm:rounded-xl transition-all duration-100 ease-in`}
             onClick={onClick}
             disabled={disabled}
             role={role}
